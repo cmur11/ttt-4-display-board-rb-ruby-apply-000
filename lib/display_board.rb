a76b2b44ef6b79ac372b2fd54036d2dd7b1a0081
Conor Murnane <conormurnane11@gmail.com>
2020-10-11 21:45:42 +0000

    Automatically backed up by Learn

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -1,2 +1,17 @@
 # Define display_board that accepts a board and prints
 # out the current state.
+
+board =[" "," "," "," "," "," "," "," "," "]
+puts board
+board = [" "," "," "," "," "," "," "," "," "]
+puts board
+
+def display_board()
+  puts "   |   |   "
+  puts "-----------"
+  puts "   |   |   "
+  puts "-----------"
+  puts "   |   |   "
+end
+
+puts display_board
